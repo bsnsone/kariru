edcd04fd9ed8e5951a4c3d690f90ffd0060a5f76
bsnsone <ggiftcardone@gmail.com>
2025-11-18 05:29:51 +0600

    Create firebase.js

---
+++ api/lib/firebase.js
@@ -0,0 +1,21 @@
+// This file initializes the Firebase CLIENT SDK
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// --- IMPORTANT ---
+// You MUST get these values from your Firebase project console.
+// Project Settings -> General -> Your apps -> Click the (</>) icon
+const firebaseConfig = {
+  apiKey: "AIzaSyARGHwuAigHV9yNMEYiJ1eyvy_s8-Ky-VA",
+  authDomain: "kanri-923d1.firebaseapp.com",
+  projectId: "kanri-923d1",
+  storageBucket: "kanri-923d1.firebasestorage.app",
+  messagingSenderId: "787454418084",
+  appId: "1:787454418084:web:af5650f5d7627a50699a31"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the Firestore database instance
+export const db = getFirestore(app);
