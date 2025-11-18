4b0f0d422c7e27d2abca8ef1313ac845fb6aca01
bsnsone <ggiftcardone@gmail.com>
2025-11-18 06:19:19 +0600

    Update firebase.js

--- api/lib/firebase.js
+++ api/lib/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyARGHwuAigHV9yNMEYiJ1eyvy_s8-Ky-VA",
   authDomain: "kanri-923d1.firebaseapp.com",
   projectId: "kanri-923d1",
-  storageBucket: "kanri-923d1.firebasestorage.app",
+  storageBucket: "kanri-923d1.appspot.com",
   messagingSenderId: "787454418084",
   appId: "1:787454418084:web:af5650f5d7627a50699a31"
 };
